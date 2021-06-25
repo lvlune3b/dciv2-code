2f184d2abee49861fd30bceb18d6b441e9867ce8
Muneeb Ali <muneeb.ali@onstak.com>
2021-06-25 16:01:21 +0500

    Completion TO DO

--- intro-python/part2/fortune_cookie.py
+++ intro-python/part2/fortune_cookie.py
@@ -34,7 +34,12 @@ def create_fortune_cookie_message(how_many_lucky_numbers: int) -> str:
     # TODO: Create a fortune cookie message by calling generate_fortune() and
     # generate_lucky_numbers() and then composing and returning the fortune
     # cookie's message.
-
+    fortune_cookie = generate_fortune()
+    #print(fortune_cookie)
+    lucky_number = generate_lucky_numbers(how_many_lucky_numbers)
+    #print(lucky_number)
+    fortune = fortune_cookie + "\n" + "Lucky Numbers:" + str(lucky_number)
+    return fortune
     raise NotImplementedError()
 
 
